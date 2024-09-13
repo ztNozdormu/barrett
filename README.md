4a7c38816ea3ff99fc80353d648697058f2f6b74
www1 <w1999wtw3537@sina.com>
2024-09-13 17:46:29 +0800

    增加proto文件夹

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # barrett
 巴雷特量化服务超级战士008
+
+
+参考https://github.com/Kixunil/tonic_lnd
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
+    tonic_build::compile_protos("proto/mk_data.proto").unwrap();
     println!("Hello, world!");
 }
