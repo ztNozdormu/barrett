f20396e6e1bb64a5bd46eabb86a51b467a709390
ztNozdormu <w1999wtw3537@sina.com>
2024-10-08 20:39:38 +0800

    动态获取交易信息

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -1,1 +1,2 @@
 pub mod bot_demo;
+pub mod bot_dynamic_demo;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use barter_bot::bot::bot_demo;
+use barter_bot::bot::{bot_demo, bot_dynamic_demo};
 
 
 #[tokio::main]
 async fn main() {
     // tonic_build::compile_protos("proto/mk_data.proto").unwrap();
-    let _ = bot_demo::bot_run().await;
+    // let _ = bot_demo::bot_run().await;
+    let _ = bot_dynamic_demo::bot_run().await;
     println!("机器人启动6666!");
 }
